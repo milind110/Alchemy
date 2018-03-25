b3798d1d976afa871680db6824f90d60283622fd
Shrinivas Kamath <shrkamat@cisco.com>
2018-03-25 12:12:43 +0530

    Used router

--- react-app/alchemy/src/App.js
+++ react-app/alchemy/src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BlogNavbar from './BlogNavbar.jsx';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Alchemy</h1>
-        </header>
-        <BlogNavbar />
-      </div>
+      return (
+          <Router>
+          <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Alchemy</h1>
+          </header>
+          <BlogNavbar />
+          </div>
+          </Router>
     );
   }
 }
