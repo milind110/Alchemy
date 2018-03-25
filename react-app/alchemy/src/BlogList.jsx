5cb68b2927de8bca81dd3cd18c52865f4ebb18b8
Shrinivas Kamath M <shrkamat@cisco.com>
2018-03-25 14:48:54 +0530

    receive blog list via redux store

--- react-app/alchemy/src/BlogList.jsx
+++ react-app/alchemy/src/BlogList.jsx
@@ -1,16 +1,44 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import Blog from './Blog.jsx'
+import {createStore} from 'redux';
+
+const store = createStore(blogsReducer);
+
+function blogsReducer(state={blogs:[]}, action) {
+    switch(action.type) {
+        default:
+        return {
+            blogs: [{
+                "title": "Blog Title 001",
+                "contents": "Blog contents.........................."
+            },
+            {
+                "title": "Blog Title 002",
+                "contents": "Blog contents.........................."
+            },
+            {
+                "title": "Blog Title 003",
+                "contents": "Blog contents.........................."
+            }
+        ]
+        };
+    }
+};
 
 class BlogList extends React.Component {
     render(){
         return(
             <table>
                 <tbody>
-                    <tr>
-                        <Blog title="Blog Title" contents="Blog Contents.....................">
+                    <p>
+                    {
+                        store.getState().blogs.map(b => (
+                        <Blog title={b.title} contents={b.contents}>
                         </Blog>
-                    </tr>
+                        ))
+                    }
+                    </p>
                 </tbody>
             </table>
         );
