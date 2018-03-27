4eae571e015561a9c4d8fc26218592f89df828d2
Shrinivas Kamath M <shrkamat@cisco.com>
2018-03-27 22:12:47 +0530

    get blog by id, http://localhost:8080/cmad-00/api/v1/blog/HelloPathParam

--- cmad-export/src/com/alchemy/web/BloggerRootResource.java
+++ cmad-export/src/com/alchemy/web/BloggerRootResource.java
@@ -3,8 +3,10 @@ package com.alchemy.web;
 import javax.servlet.http.HttpServlet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 
 /**
  * Servlet implementation class BloggerRootResource
@@ -12,15 +14,22 @@ import javax.ws.rs.core.Response;
 @Path("/v1")
 public class BloggerRootResource extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public BloggerRootResource() {
-        super();
-    }
 
-    @GET
-    @Path("/blog")
-    public Response getBlog()
-    {
-    	return Response.ok().entity("Hello, Blogger").build();
-    }
+	public BloggerRootResource() {
+		super();
+	}
+
+	@GET
+	@Path("/blog")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response getBlog() {
+		return Response.ok().entity("Hello, Blogger").build();
+	}
+
+	@GET
+	@Path("/blog/{blogId}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response getBlog(@PathParam("blogId") String blogId) {
+		return Response.ok().entity("GET blogId=" + blogId).build();
+	}
 }
